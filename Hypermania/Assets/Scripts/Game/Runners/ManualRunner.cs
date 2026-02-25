a96717c236ac97f0f0711332540dc8fe8302c14c
Raymond Bian <raybbian@gmail.com>
2026-02-25 11:39:41 -0500

    add input to manual runner

--- Hypermania/Assets/Scripts/Game/Runners/ManualRunner.cs
+++ Hypermania/Assets/Scripts/Game/Runners/ManualRunner.cs
@@ -1,18 +1,42 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Game.Runners
 {
     public class ManualRunner : SingleplayerRunner
     {
+        [SerializeField]
+        private Key _advanceKey;
+
+        [SerializeField]
+        private float _holdS;
+
+        private float _curHoldS = 0;
+
         public override void Poll(float deltaTime)
         {
             if (!_initialized)
             {
                 return;
             }
-            if (Input.GetKeyDown(KeyCode.RightArrow))
+            _inputBuffer.Saturate();
+            if (Keyboard.current[Key.RightArrow].wasPressedThisFrame)
             {
                 GameLoop();
+                _inputBuffer.Clear();
+            }
+            if (Keyboard.current[Key.RightArrow].isPressed)
+            {
+                _curHoldS += deltaTime;
+                if (_curHoldS >= _holdS)
+                {
+                    GameLoop();
+                    _inputBuffer.Clear();
+                }
+            }
+            else
+            {
+                _curHoldS = 0;
             }
         }
     }
